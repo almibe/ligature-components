9fd5d15879dc2d4ad3208a639912c3ad760e4aec
Alex Michael Berry <alexmiberry@gmail.com>
2023-10-06 22:46:46 -0400

    avoiding shadow dom

--- src/wander-lang.ts
+++ src/wander-lang.ts
@@ -4,12 +4,16 @@ import { customElement } from 'lit/decorators.js'
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/sml';
-import style from 'highlight.js/styles/dark.css?inline';
+import style from '../node_modules/highlight.js/styles/default.css';
 
 hljs.registerLanguage('sml', javascript);
 
 @customElement('wander-lang')
 export class WanderLang extends LitElement {
+  createRenderRoot() {
+    return this;
+  }
+  
   render() {
     const action = this.attributes.getNamedItem("action");
     if (action == null ) {
