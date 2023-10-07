c985fd79951e7c80b671057c716ecbd86ab75fe4
Alex Michael Berry <alexmiberry@gmail.com>
2023-10-06 20:29:23 -0400

    experimenting with highlight.js, #26

--- src/wander-lang.ts
+++ src/wander-lang.ts
@@ -1,30 +1,39 @@
+import { run, introspect } from '@wander-lang/wander';
 import { LitElement, html, unsafeCSS } from 'lit'
 import { customElement } from 'lit/decorators.js'
-import { run, introspect } from '@wander-lang/wander';
-import style from "prismjs/themes/prism-coy.css?inline";
-import Prism from "prismjs";
-import "prismjs/components/prism-sml"
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/sml';
+import style from 'highlight.js/styles/dark.css?inline';
+
+hljs.registerLanguage('sml', javascript);
 
 @customElement('wander-lang')
 export class WanderLang extends LitElement {
   render() {
     const action = this.attributes.getNamedItem("action");
     if (action == null ) {
-      return html`<pre><code class="language-sml">${this.textContent!!}</code></pre>`
+      const result = hljs.highlight(
+        this.textContent,
+        { language: 'sml' }
+      ).value;
+      return html`<pre><code class="language-sml">${unsafeHTML(result)}</code></pre>`
     } else if (action.value == "run") {
       return html`<pre>${JSON.stringify(run(this.textContent!!))}</pre>`
     } else if (action.value == "introspect") {
       return html`<pre>${JSON.stringify(introspect(this.textContent!!))}</pre>`
     } else if (action.value == "display") {
-      const result = Prism.highlight(this.textContent, Prism.languages.sml);
+      const result = hljs.highlight(
+        this.textContent,
+        { language: 'sml' }
+      ).value;
       return html`<pre><code class="language-sml">${unsafeHTML(result)}</code></pre>`
     } else {
       throw new DOMException("Unknown Action " + action.value);
     }
   }
 
-  static styles = unsafeCSS(style);
+  static styles = unsafeCSS(style + "\n * { background-color: #111111; color: #DDDDDD }");
 }
 
 declare global {
