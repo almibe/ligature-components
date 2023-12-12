1663b98e28ae2d42c42c147883c3997301ad1f80
Alex Michael Berry <alexmiberry@gmail.com>
2023-12-12 03:29:43 -0500

    minor updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
 import { defineConfig } from 'vite'
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
 import postcssLit from 'rollup-plugin-postcss-lit';
 
 export default defineConfig({
   plugins: [
-    wasm(),
-    topLevelAwait(),
     postcssLit(),
   ]
 });
-
-// old config
-// export default defineConfig({
-//     build: {
-//       lib: {
-//         entry: 'src/wander-lang.ts',
-//         formats: ['es'],
-//       },
-//       rollupOptions: {
-//         external: /^lit/,
-//       },
-//     },
-//   })
-  
