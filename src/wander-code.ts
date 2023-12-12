6d1c2914a15a0419b0f254cb67f6b3eb6225cc43
Alex Michael Berry <alexmiberry@gmail.com>
2023-12-12 03:32:03 -0500

    removing old code

--- src/wander-code.ts
+++ src/wander-code.ts
@@ -1,4 +1,4 @@
-import { run, introspect } from '@wander-lang/wander';
+import { execute, highlight } from '@wander-lang/wander';
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
@@ -9,7 +9,12 @@ export class WanderCode extends LitElement {
   }
   
   @property()
-  script: string = "";
+  declare script: string;
+
+  constructor() {
+    super();
+    this.script = "";
+  }
 
   connectedCallback(): void {
     super.connectedCallback();
@@ -25,53 +30,11 @@ export class WanderCode extends LitElement {
 }
 
 function runScript(input: string) {
-  let res = run(input);
-  if (res.string.Ok) {
-    return html`<span class="result">Result:${res.string.Ok}`;
+  let res = execute(input);
+  if (typeof res === 'string' && res.startsWith("Error:")) {
+    return html`<span class="error">${res}</span>`;
   } else {
-    return html`<span class="error">Error: ${res.string.Err}</span>`;
-  }
-}
-
-function highlight(input: string) {
-  let intro = introspect(input);
-  if (intro.Ok) {
-    let tokens = intro.Ok.tokens_ws;
-    return html`${tokens.map((token) => highlight_token(token))}`;
-  } else {
-    return html`<span class="error">Error: ${intro.Err}</span>`;
-  }
-}
-
-const keywords = ["Let", "In", "End", "Val", "If", "Else", "When"];
-
-const syntax = {
-  OpenParen: "(",
-  CloseParen: ")",
-  SingleQuote: "'",
-  EqualSign: "=",
-  OpenSquare: "[",
-  CloseSquare: "]",
-  OpenBrace: "{",
-  CloseBrace: "}",
-  Lambda: "\\",
-  Arrow: "->",
-  Colon: ":",
-}
-
-function highlight_token(token) {
-  if (token.WS) {
-    return html`${token.WS}`
-  } else if (keywords.includes(token)) {
-    return html`<span class="keyword">${token.toLowerCase()}</span>`
-  } else if (token in syntax) {
-    return html`<span class="syntax">${syntax[token]}</span>`
-  } else if (token.Name) {
-    return html`<span class="name">${token.Name}<span>`
-  } else if (token.String) {
-    return html`<span class="string">"${token.String}"<span>`
-  } else {
-    throw new DOMException(`Unhandled ${JSON.stringify(token)}`);
+    return html`<span class="result"><pre>${JSON.stringify(res)}</pre></span>`;
   }
 }
 
