d6c67f4155524f5f4ddbe98a44d9cae6280c0d84
Alex Michael Berry <alexmiberry@gmail.com>
2024-01-06 06:21:06 -0500

    removing lit, closes #31

--- src/wander-code.ts
+++ src/wander-code.ts
@@ -1,45 +1,19 @@
 import { execute, highlight } from '@wander-lang/wander';
-import { LitElement, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
 
-@customElement('wander-code')
-export class WanderCode extends LitElement {
-  createRenderRoot() {
-    return this;
-  }
-  
-  @property()
-  declare script: string;
+wanderCode("code", "x = 5")
 
-  constructor() {
-    super();
-    this.script = "";
-  }
-
-  connectedCallback(): void {
-    super.connectedCallback();
-    this.script = this.textContent!!;
-    this.textContent = "";
-  }
-
-  render() {
-    const highlightResult = JSON.parse(JSON.stringify(highlight(this.script))).map(value => html`<span class="${value[0]}">${value[1]}</span>`);
-    const runResult = runScript(this.script);
-    return html`<pre><code>${highlightResult}</code></pre><hr><pre><code>${runResult}</code></pre>`;
-  }
+export function wanderCode(id, script) {
+  const element = document.getElementById(id);
+  const highlightResult = JSON.parse(JSON.stringify(highlight(script))).map(value => `<span class="${value[0]}">${value[1]}</span>`);
+  const runResult = runScript(script);
+  element.innerHTML = `<pre><code>${highlightResult}</code></pre><hr><pre><code>${runResult}</code></pre>`;
 }
 
 function runScript(input: string) {
   let res = execute(input);
   if (typeof res === 'string' && res.startsWith("Error:")) {
-    return html`<span class="error">${res}</span>`;
+    return `<span class="error">${res}</span>`;
   } else {
-    return html`<span class="result"><pre>${JSON.stringify(res)}</pre></span>`;
-  }
-}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'wander-code': WanderCode
+    return `<span class="result"><pre>${JSON.stringify(res)}</pre></span>`;
   }
 }
--- src/wander-editor.ts
+++ src/wander-editor.ts
@@ -1,60 +1,31 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property, query } from 'lit/decorators.js'
 import { EditorView, basicSetup } from "codemirror";
 import { ViewUpdate, keymap } from "@codemirror/view"
 import { indentWithTab } from "@codemirror/commands"
 import { language } from '@codemirror/language';
 import { WanderLanguage } from './wander-code-mirror';
 
-@customElement('wander-editor')
-export class WanderEditor extends LitElement {
-  @query('#editor')
-  _editorNode: Element;
+initializeEditor("editor", "x = 6")
 
-  @property()
-  text: string = "";
-
-  render() {
-    return html`<div id="editor" class="code"></div>`
-  }
-
-  connectedCallback(): void {
-    super.connectedCallback();
-    this.text = this.textContent!!;
-    this.textContent = "";
-    this.initializeEditor();
-  }
-
-  static styles = css``;
-
-  async initializeEditor() {
-    await this.updateComplete;
-    let inputEditor: EditorView;
-  
-    inputEditor = new EditorView({
-      doc: this.text,
-      extensions: [
-        language.of(WanderLanguage),
-        EditorView.theme({
-          "&": {height: "300px", overflow: "auto", resize: "vertical"},
-          ".cm-scroller": {overflow: "auto"}
-        }),
-        EditorView.updateListener.of((v: ViewUpdate) => {
-          if (v.docChanged) {
-            this.text = v.state.doc.toString();
-          }
-        }),
-        basicSetup,
-        keymap.of([indentWithTab]), 
-        ],
-        parent: this._editorNode,
-      });
-      inputEditor.focus();
-  }
-}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'wander-editor': WanderEditor
-  }
+export function initializeEditor(id, text) {
+  let inputEditor: EditorView;
+  const element = document.getElementById(id)
+  inputEditor = new EditorView({
+    doc: text,
+    extensions: [
+      language.of(WanderLanguage),
+      EditorView.theme({
+        "&": {height: "300px", overflow: "auto", resize: "vertical"},
+        ".cm-scroller": {overflow: "auto"}
+      }),
+      EditorView.updateListener.of((v: ViewUpdate) => {
+        if (v.docChanged) {
+          //this.text = v.state.doc.toString();
+        }
+      }),
+      basicSetup,
+      keymap.of([indentWithTab]), 
+      ],
+      parent: element,
+    });
+    inputEditor.focus();
 }
