22398ab6226ab4a233cb1f114bc0855652dbc85e
Alex Michael Berry <alexmiberry@gmail.com>
2024-02-18 20:13:29 -0500

    adding navigo for routing, closes #69 nice

--- wander-lab/src/client/main.ts
+++ wander-lab/src/client/main.ts
@@ -3,35 +3,43 @@ import { printResult, run } from "../../../wander/src/interpreter";
 import { parse } from "../../../wander/src/parser";
 import "./style.css";
 import { initializeRepl } from "wander-components/src/repl/repl.ts"
+import Navigo from 'navigo';
 
-initializeRepl("app", async (script) => {
-    if (script.startsWith("!")) {
-        const finalScript = `{action="inspect", script=${JSON.stringify(script.slice(1))}}`
-        console.log("Running", finalScript);
-        let result = await fetch("/wander", {
-            method: "POST",
-            body: finalScript
-        });
-        let resultText = await result.text();
-        return resultText;
-    } else {
-        const finalScript = `{action="run", script=${JSON.stringify(script)}}`
-        console.log("Running", finalScript);
-        let result = await fetch("/wander", {
-            method: "POST",
-            body: finalScript
-        });
-        let resultText = await result.text();
-        return resultText;
-    }
+const router = new Navigo('/');
+
+router.on('/term', function () {
+    initializeRepl("app", async (script) => {
+        if (script.startsWith("!")) {
+            const finalScript = `{action="inspect", script=${JSON.stringify(script.slice(1))}}`
+            console.log("Running", finalScript);
+            let result = await fetch("/wander", {
+                method: "POST",
+                body: finalScript
+            });
+            let resultText = await result.text();
+            return resultText;
+        } else {
+            const finalScript = `{action="run", script=${JSON.stringify(script)}}`
+            console.log("Running", finalScript);
+            let result = await fetch("/wander", {
+                method: "POST",
+                body: finalScript
+            });
+            let resultText = await result.text();
+            return resultText;
+        }
+        
+        // printResult(run(script));
     
-    // printResult(run(script));
-
-    // if (script.startsWith("!")) {
-    //     script = script.slice(1);
-    //     return JSON.stringify(parse(script), null, 2);
-    // } else {
-    //     const result = printResult(run(script, std()));
-    //     return result;
-    // }
+        // if (script.startsWith("!")) {
+        //     script = script.slice(1);
+        //     return JSON.stringify(parse(script), null, 2);
+        // } else {
+        //     const result = printResult(run(script, std()));
+        //     return result;
+        // }
+    });
+        // do something
 });
+
+router.resolve();
