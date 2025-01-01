6949371755065a8616161d10ef8e03a8134dab24
Alex Michael Berry <alexmiberry@gmail.com>
2025-01-01 03:30:33 -0500

    starting on purescript impl

---
+++ packages/ligature/index.js
@@ -0,0 +1,161 @@
+// output/Control.Semigroupoid/index.js
+var semigroupoidFn = {
+  compose: function(f) {
+    return function(g) {
+      return function(x) {
+        return f(g(x));
+      };
+    };
+  }
+};
+
+// output/Control.Category/index.js
+var identity = function(dict) {
+  return dict.identity;
+};
+var categoryFn = {
+  identity: function(x) {
+    return x;
+  },
+  Semigroupoid0: function() {
+    return semigroupoidFn;
+  }
+};
+
+// output/Effect/foreign.js
+var pureE = function(a) {
+  return function() {
+    return a;
+  };
+};
+var bindE = function(a) {
+  return function(f) {
+    return function() {
+      return f(a())();
+    };
+  };
+};
+
+// output/Control.Apply/index.js
+var apply = function(dict) {
+  return dict.apply;
+};
+
+// output/Control.Applicative/index.js
+var pure = function(dict) {
+  return dict.pure;
+};
+var liftA1 = function(dictApplicative) {
+  var apply2 = apply(dictApplicative.Apply0());
+  var pure1 = pure(dictApplicative);
+  return function(f) {
+    return function(a) {
+      return apply2(pure1(f))(a);
+    };
+  };
+};
+
+// output/Control.Bind/index.js
+var bind = function(dict) {
+  return dict.bind;
+};
+
+// output/Control.Monad/index.js
+var ap = function(dictMonad) {
+  var bind2 = bind(dictMonad.Bind1());
+  var pure2 = pure(dictMonad.Applicative0());
+  return function(f) {
+    return function(a) {
+      return bind2(f)(function(f$prime) {
+        return bind2(a)(function(a$prime) {
+          return pure2(f$prime(a$prime));
+        });
+      });
+    };
+  };
+};
+
+// output/Effect/index.js
+var $runtime_lazy = function(name, moduleName, init) {
+  var state = 0;
+  var val;
+  return function(lineNumber) {
+    if (state === 2)
+      return val;
+    if (state === 1)
+      throw new ReferenceError(name + " was needed before it finished initializing (module " + moduleName + ", line " + lineNumber + ")", moduleName, lineNumber);
+    state = 1;
+    val = init();
+    state = 2;
+    return val;
+  };
+};
+var monadEffect = {
+  Applicative0: function() {
+    return applicativeEffect;
+  },
+  Bind1: function() {
+    return bindEffect;
+  }
+};
+var bindEffect = {
+  bind: bindE,
+  Apply0: function() {
+    return $lazy_applyEffect(0);
+  }
+};
+var applicativeEffect = {
+  pure: pureE,
+  Apply0: function() {
+    return $lazy_applyEffect(0);
+  }
+};
+var $lazy_functorEffect = /* @__PURE__ */ $runtime_lazy("functorEffect", "Effect", function() {
+  return {
+    map: liftA1(applicativeEffect)
+  };
+});
+var $lazy_applyEffect = /* @__PURE__ */ $runtime_lazy("applyEffect", "Effect", function() {
+  return {
+    apply: ap(monadEffect),
+    Functor0: function() {
+      return $lazy_functorEffect(0);
+    }
+  };
+});
+
+// output/Effect.Class/index.js
+var monadEffectEffect = {
+  liftEffect: /* @__PURE__ */ identity(categoryFn),
+  Monad0: function() {
+    return monadEffect;
+  }
+};
+var liftEffect = function(dict) {
+  return dict.liftEffect;
+};
+
+// output/Effect.Console/foreign.js
+var log = function(s) {
+  return function() {
+    console.log(s);
+  };
+};
+
+// output/Effect.Class.Console/index.js
+var log2 = function(dictMonadEffect) {
+  var $67 = liftEffect(dictMonadEffect);
+  return function($68) {
+    return $67(log($68));
+  };
+};
+
+// output/Main/index.js
+var log3 = /* @__PURE__ */ log2(monadEffectEffect);
+var main = function __do() {
+  log3("\u{1F35D}")();
+  return log3("You should add some tests.")();
+};
+export {
+  main
+};
