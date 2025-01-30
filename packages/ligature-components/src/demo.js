dd72296c7b7c9e9964604349528198f5a8d2eb0f
Alex Michael Berry <alexmiberry@gmail.com>
2025-01-29 21:41:12 -0500

    minor renamings + bumping versions

---
+++ packages/ligature-components/src/demo.js
@@ -0,0 +1,19 @@
+import './style.css'
+import { runWithActions } from "@ligature/ligature"
+import { showEditor } from './editor/editor.js'
+//import { appendStackText, appendText } from './text/text'
+import { appendTable } from './table/table.js'
+import { appendGraph } from './graph/graph.js'
+import { createComponentActions } from './actions.js'
+
+let initalScript = `{a b c} display-table`
+
+let editor = showEditor(document.querySelector("#editor"), initalScript)
+
+let actions = createComponentActions(document.querySelector("#results"))
+
+document.querySelector("#runButton")?.addEventListener("click", () => {
+    document.querySelector("#results").innerHTML = ""
+
+    runWithActions(actions, editor.state.doc.toString())
+})
