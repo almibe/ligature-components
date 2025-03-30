af4ced8e1370832f44a90bb9b1353a91133a25b5
Alex Michael Berry <alexmiberry@gmail.com>
2025-03-30 05:14:23 -0400

    supporting display canvas

--- packages/ligature-components/src/demo.js
+++ packages/ligature-components/src/demo.js
@@ -2,12 +2,17 @@ import { runScript, showEditor } from './lib'
 
 import './style.css'
 
-let initalScript = `{a b c}`
+let initalScript = `
+{ display canvas 
+  width 200 
+  height 200 
+  instructions [
+    [fill-style blue]
+    [fill-rect 10 10 150 100]]}`
 
 let editor = showEditor(document.querySelector("#editor"), initalScript)
 
 document.querySelector("#runButton")?.addEventListener("click", () => {
 //    document.querySelector("#results").innerHTML = ""
     runScript(editor.state.doc.toString(), document.querySelector("#results"))
-//    document.querySelector("#results").innerHTML = printResult(res)
 })
