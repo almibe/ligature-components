40963f6f812856bae3b45c68c06f9892fe446d14
Alex Michael Berry <alexmiberry@gmail.com>
2025-04-06 03:38:16 -0400

    removing old code

--- packages/ligature-components/src/text/text.js
+++ packages/ligature-components/src/text/text.js
@@ -9,13 +9,3 @@ export function appendText(element, value) {
         element.appendChild(pre)
     }
 }
-
-// export function appendStackText(element, stack) {
-//     if (element != null) {
-//         const pre = document.createElement("pre")
-//         const code = document.createElement("code")
-//         pre.appendChild(code)
-//         code.textContent = printStack(stack)
-//         element.appendChild(pre)
-//     }
-// }
