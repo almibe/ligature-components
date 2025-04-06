e2fa4179c4e9835dc9406d58ae0ff09b3e2ec32d
Alex Michael Berry <alexmiberry@gmail.com>
2025-04-06 01:02:11 -0400

    removing old code

--- packages/ligature-components/src/lib.js
+++ packages/ligature-components/src/lib.js
@@ -25,38 +25,3 @@ export function runScript(script, element) {
     appendText(element, res)
   }
 }
-
-// export function createComponentFns(div) {
-//   let actions = new Map()
-
-//   actions.set("display-text", (args) => {
-//     if (args.length == 1) {
-//       appendText(div, args[0])
-//       return ok(tail)
-//     } else {
-//       throw "display-text requires a single value"
-//     }
-//   })
-
-//   actions.set("display-table", (args) => {
-//     if (args.length == 1) {
-//       let network = networkToJs(args[0])
-//       appendTable(div, network)
-//       return ok(tail)
-//     } else {
-//       throw "display-table requires a network value"
-//     }
-//   })
-
-//   actions.set("display-graph", (args) => {
-//     if (args.length == 1) {
-//       let network = networkToJs(args[0])
-//       appendGraph(div, network)
-//       return ok(tail)
-//     } else {
-//       throw "display-graph requires a network value"
-//     }
-//   })
-
-//   return actions
-// }
